6531123dcb31915cb38e011878b7b496993cf8dc
Log <laylog@outlook.com>
2016-09-10 15:08:37 +0800

    Added File access files

---
+++ ReadAndWrite/src/AccessAndWrite.java
@@ -0,0 +1,54 @@
+import java.io.DataOutput;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class AccessAndWrite {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		File f = new File("/Users/hyleung/Self/Code/","Test.txt");
+		byte[] bf = new byte[(int)f.length()];
+		RandomAccessFile rf = new RandomAccessFile (f, "rw");
+		
+		
+		try{
+			rf.read(bf);
+			for(int i=0;i<bf.length; i++)
+			{
+				bf[i] = (byte) (bf[i]+12);
+			}
+//			rf.setLength(0);
+//			rf.write(bf);
+//			rf.seek(3);
+//			System.out.println((char)rf.read());
+			
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+		}
+		
+		File f1 = new File("/Users/hyleung/Self/Code/","Test.txt");
+		File f2 = new File("/Users/hyleung/Self/Code/","Decode.txt");
+		RandomAccessFile rf2 = new RandomAccessFile (f2, "rw");
+		
+		byte[] bf1 = new byte[(int)f1.length()];
+		try{
+			
+			for(int i=0;i<bf1.length; i++)
+			{
+				bf1[i] = (byte) (bf[i]-12);
+			}
+			
+			rf2.setLength(0);
+			
+			rf2.write(bf1);
+			
+			
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+}
